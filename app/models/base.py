a9b9cd480a258f50c7277c8da837066f2c674906
Faakhir30 <zahidfaakhir@gmail.com>
2025-05-18 23:39:41 +0500

    Add base models
    Signed-off-by: Faakhir30 <zahidfaakhir@gmail.com>

---
+++ app/models/base.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime
+
+Base = declarative_base()
+
+
+class BaseModel:
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False
+    )
